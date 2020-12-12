9d5f6f6f1fb58cd228b317a03ad4f5ab1e6aae66
Jeremy Bertin <jeremy.bertin76@hotmail.fr>
2020-12-12 15:48:47 +0100

    Many improvements

--- assets/js/scroll-smooth.js
+++ assets/js/scroll-smooth.js
@@ -1,41 +1,31 @@
 
-    $('a[href^="#"]').on('click',function (e) {
-     if (window.matchMedia('(min-width: 1200px)').matches) {
-            var target = this.hash,
-        $target = $(target);
+   $('a[href^="#"]').on('click',function (e) {
+    if (window.matchMedia('(min-width: 1200px)').matches) {
+         var target = this.hash,
+     $target = $(target);
 
-       $('html, body').stop().animate({
-         'scrollTop': $target.offset().top - 110
-        }, 900, 'swing', function () {
-         window.location.hash = target;
-        });
-      } 
-        else if (window.matchMedia('(min-width: 993px)').matches) {
-            var target = this.hash,
-        $target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top - 90
+     }, 1200, 'swing', function () {
+      window.location.hash = target;
+     });
+   } else if (window.matchMedia('(min-width: 992px)').matches) {
+         var target = this.hash,
+     $target = $(target);
 
-       $('html, body').stop().animate({
-         'scrollTop': $target.offset().top - 420
-        }, 900, 'swing', function () {
-         window.location.hash = target;
-        });
-      } else if (window.matchMedia('(max-width: 992px)').matches) {
-        var target = this.hash,
-        $target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top - 60
+     }, 1200, 'swing', function () {
+      window.location.hash = target;
+     });
+   } else if (window.matchMedia('(max-width: 991.99px)').matches) {
+         var target = this.hash,
+     $target = $(target);
 
-      $('html, body').stop().animate({
-        'scrollTop': $target.offset().top - 60
-      }, 900, 'swing', function () {
-        window.location.hash = target;
-      });
-      } else if (window.matchMedia('(max-width: 414px)').matches) {
-            var target = this.hash,
-        $target = $(target);
-
-       $('html, body').stop().animate({
-         'scrollTop': $target.offset().top - 300
-        }, 900, 'swing', function () {
-         window.location.hash = target;
-        });
-      }
-    });
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+     }, 1200, 'swing', function () {
+      window.location.hash = target;
+     });
+   }
+ });
